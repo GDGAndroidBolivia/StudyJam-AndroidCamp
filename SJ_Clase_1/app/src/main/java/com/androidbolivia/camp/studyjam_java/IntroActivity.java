91a79778c7102c21120360c9d4a4352a7e1c60d3
Alan D. Ortiz <alandaviov@gmail.com>
2017-11-16 13:06:18 -0400

    adding new folders for all the classes

---
+++ SJ_Clase_1/app/src/main/java/com/androidbolivia/camp/studyjam_java/IntroActivity.java
@@ -0,0 +1,39 @@
+package com.androidbolivia.camp.studyjam_java;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+public class IntroActivity extends AppCompatActivity {
+    private ImageView ivLogo;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro);
+        ivLogo = (ImageView) findViewById(R.id.brand);
+
+        Animation animacion = AnimationUtils.loadAnimation(
+                getApplicationContext(), R.anim.splashscreen);
+        ivLogo.startAnimation(animacion);
+        animacion.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+            }
+        });
+    }
+}
